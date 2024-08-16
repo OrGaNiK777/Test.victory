d62cc9b83de9be10902060fbb1e7554a6f2a98a0
OrGaNiK <OrGaNiK777@yandex.ru>
2024-08-16 16:56:37 +0500

    16.08.24

--- index.js
+++ index.js
@@ -59,3 +59,10 @@ document.getElementById('nextBtn').addEventListener('click', () => {
 
 // Начальное отображение карточек
 updateCardsDisplay()
+
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+  e.style.setProperty('--value', e.value);
+  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+  e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+}
